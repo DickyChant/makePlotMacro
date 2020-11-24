741f46e4f1c6d45e5b6e6d413e175b89941d6f9b
DickyChant <761337540@qq.com>
2020-11-24 21:40:22 +0800

    correct selection rules

--- DirectlyDraw.C
+++ DirectlyDraw.C
@@ -3,50 +3,65 @@
 
 using std::ifstream;
 
-const char* name_template = "%s/HeavyN_SSWW_MuMu_onlyVmuN1_M%d*.root";
+const char* name_template = "%s/HeavyN_SSWW_MuMu_onlyVmuN1_M%d_*.root";
 
-string SetRules(const char* selrule_path = "SelectionRules.dat"){
-    ifstream infile(selrule_path);
+TString SetRules(const char* selrule_path = "SelectionRules.dat"){
+   ifstream infile(selrule_path);
 
-    const char* empty = "";
 
-    char* readin;
-    char* tmp = nullptr;
-    
-    while(!infile.eof()){
-        infile>>readin;
-        if(tmp!=nullptr) {
-            tmp = Form("(%s)&&(%s)",tmp,readin);
-        }
-        else{
-            tmp = Form("(%s)",readin);
-        }
-    }
+   TString readin;
+   TString tmp = "";
+   
+   cout<<"Output of Setting Selection\n";
+   
+   while(!infile.eof()){
+       infile>>readin;
+       cout<<readin<<endl;
+       if(tmp.Sizeof()!=1) {
+           cout<<tmp.Sizeof()<<endl;
+           tmp += Form("&(%s)",readin.Data());
+       }
+       else{
+           tmp = "(" + readin + ")";
+       }
+       cout<<tmp<<endl;
+   }
+	infile.close();
 
-    return tmp;
+    cout<<"End of Setting Selection\n";
+	
+   return tmp;
 }
+
 
 void DirectlyDraw( const char* root_file_path,const char* funcname  = "GenJet_pt"){
 
     TChain* chains[9];
 
-    auto c1 = new TCanvas("c1","c1",800,600);  
+
+	auto c1 = new TCanvas("c1","c1",800,600);  
 
     int Masses[9] = {1500,1750,2000,2500,5000,7500,10000,15000,20000};
 
+
+	auto SelRules = SetRules();
+
+
     for (int i = 0 ; i < 9 ; i++){
         chains[i] = new TChain("Events");
-        chains[i]->Add(Form(name_template,root_file_path,Masses[i])) ;
+        cout<<chains[i]->Add(Form(name_template,root_file_path,Masses[i]))<<endl ;
+		
         cout<<     Form(name_template,root_file_path,Masses[i])<<endl;  
     }
 
-    chains[0]->Draw(funcname);
-
-    auto SelRules = SetRules();
     
-    for (int i = 1 ; i < 9 ;i ++){
-        
-        chains[i]->Draw(funcname,SelRules.c_str(),"same");
+    for (int i = 0 ; i < 9 ;i ++){
+       	if (i == 0){
+		chains[i]->Draw(funcname,SelRules.Data());
+		} 
+		else{
+        chains[i]->Draw(funcname,SelRules.Data(),"same");
+		}
         
     }
 
@@ -56,7 +71,7 @@ void DirectlyDraw( const char* root_file_path,const char* funcname  = "GenJet_pt
 
 
 
-	c1->SaveAs("DirectPlot.pdf");
+	c1->SaveAs("plots/DirectPlot.pdf");
 
 
 }
